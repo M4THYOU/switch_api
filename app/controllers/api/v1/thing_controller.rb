999d628174e7d88adbb1749005dcb16950808f85
Matt Wolfe <m5wolfe@uwaterloo.ca>
2021-01-22 09:19:53 -0500

    get device state

--- app/controllers/api/v1/thing_controller.rb
+++ app/controllers/api/v1/thing_controller.rb
@@ -1,56 +1,30 @@
 class Api::V1::ThingController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
-  # https://a2eis0wug3zm6u.iot.us-east-2.amazonaws.com/things/esp8266/shadow
-  #
-  # a2eis0wug3zm6u-ats.iot.us-east-2.amazonaws.com # use this one for device itself I think
-  def update
-    things = Thing.all
-    puts "fuck"
-
-    signer = Aws::Sigv4::Signer.new(
-      service: 'iotdata',
-      region: 'us-east-2',
-      # static credentials
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    )
-
-    host = 'https://a2eis0wug3zm6u.iot.us-east-2.amazonaws.com'
-    thing_name = 'esp8266'
-    uri_string = host + '/things/' + thing_name + '/shadow'
-
-    signature = signer.sign_request(
-      http_method: 'GET',
-      url: uri_string,
-      body: {}.to_json
-    )
-    uri = URI(uri_string)
-
-    Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
-      request = Net::HTTP::Get.new uri
-      request.body = {}.to_json
-
-      request['Host'] = signature.headers['host']
-      request['X-Amz-Date'] = signature.headers['x-amz-date']
-      request['X-Amz-Security-Token'] = signature.headers['x-amz-security-token']
-      request['X-Amz-Content-Sha256']= signature.headers['x-amz-content-sha256']
-      request['Authorization'] = signature.headers['authorization']
-      request['Content-Type'] = 'application/json'
-      response = http.request request
-      puts response.body
+    def index
+        Thing.all
     end
 
-    render json: things, status: 200
-  end
+    def show
+        thing_name = params[:id]
+        com = IotCoreCom.new thing_name
+        state = com.get_state
+        thing = Thing.find_by aws_name: thing_name
+        thing_hash = thing.as_json
+        thing_hash[:state] = state
+        render json: thing_hash, status: 200
+    end
 
-  private
+    # https://a2eis0wug3zm6u.iot.us-east-2.amazonaws.com/things/esp8266/shadow
+    #
+    # a2eis0wug3zm6u-ats.iot.us-east-2.amazonaws.com # use this one for device itself I think
+    def update
+        puts "fuck"
 
-  def update_params
-  end
+        render json: things, status: 200
+    end
+
+    private
+
+    def update_params
+    end
 
 end
---
+++ app/services/iot_core_com.rb
@@ -0,0 +1,44 @@
+class IotCoreCom
+
+    def initialize(thing_name)
+        @thing_name = thing_name
+        @signer = Aws::Sigv4::Signer.new(
+            service: 'iotdata',
+            region: 'us-east-2',
+            access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+            secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+        )
+        host = 'https://a2eis0wug3zm6u.iot.us-east-2.amazonaws.com'
+        @base_uri = host + '/things/' + thing_name + '/shadow'
+    end
+
+    def get_state
+        signature = @signer.sign_request(
+            http_method: 'GET',
+            url: @base_uri,
+            body: {}.to_json
+        )
+        uri = URI(@base_uri)
+        result = ''
+        Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
+            request = Net::HTTP::Get.new uri
+            request.body = {}.to_json
+
+            request['Host'] = signature.headers['host']
+            request['X-Amz-Date'] = signature.headers['x-amz-date']
+            request['X-Amz-Security-Token'] = signature.headers['x-amz-security-token']
+            request['X-Amz-Content-Sha256']= signature.headers['x-amz-content-sha256']
+            request['Authorization'] = signature.headers['authorization']
+            request['Content-Type'] = 'application/json'
+            response = http.request request
+            result = response.body
+        end
+        if result.blank?
+            {:error => 'no response found.'}
+        else
+            state = JSON.parse result
+            state["state"]["delta"]
+        end
+    end
+
+end
