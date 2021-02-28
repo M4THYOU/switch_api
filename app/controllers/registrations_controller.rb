325274e2b098bfc7eb840c1abfacf0bf5ac34db5
Matt Wolfe <m5wolfe@uwaterloo.ca>
2021-02-28 11:31:46 -0500

    can register even if invited

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -15,4 +15,21 @@ class RegistrationsController < Devise::RegistrationsController
         # sign up parameters.
         devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     end
+
+    def build_resource(hash = {})
+        email = hash[:email]
+        u = User.find_by(email: email)
+        if u.nil?
+            self.resource = User.new_with_session(hash, session)
+        elsif u.first_name.blank? and u.last_name.blank? and u.confirmation_token.blank? and u.confirmed_at.blank?
+            hash[:invitation_token] = nil
+            hash[:invitation_created_at] = nil
+            hash[:invitation_sent_at] = nil
+            hash[:invited_by_type] = nil
+            hash[:invited_by_id] = nil
+            u.update(hash)
+            u.send_confirmation_instructions
+            self.resource = u
+        end
+    end
 end
